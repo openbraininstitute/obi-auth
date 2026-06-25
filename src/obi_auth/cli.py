80fb4bdc11d80b60d6fca677a8e279d535af8187
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-06-25 16:48:55 +0200

    Improve CLI experience

--- src/obi_auth/cli.py
+++ src/obi_auth/cli.py
@@ -1,8 +1,9 @@
 #!/bin/env python3
 """CLI for obi-auth."""
 
+import json
 import logging
-import pprint
+import sys
 
 import click
 
@@ -41,8 +42,31 @@ def get_token(environment, auth_mode, show_decoded, show_user_info):
     access_token = obi_auth.get_token(environment=environment, auth_mode=AuthMode(auth_mode))
     print(access_token)
 
-    if show_decoded:
-        pprint.pprint(obi_auth.get_token_info(access_token))
 
-    if show_user_info:
-        pprint.pprint(obi_auth.get_user_info(access_token, environment=environment))
+@main.command()
+@click.argument("access_token", required=False)
+def decode_token(access_token):
+    """Decode token from argument or stdin."""
+    if not access_token:
+        access_token = sys.stdin.read().strip()
+
+    if not access_token:
+        raise click.ClickException("No access token provided via argument or stdin")
+
+    print(json.dumps(obi_auth.get_token_info(access_token), indent=2))
+
+
+@main.command()
+@click.option(
+    "--auth-mode",
+    "-m",
+    default="pkce",
+    help="Authentication method",
+    type=click.Choice([mode.value for mode in AuthMode]),
+)
+@click.option("--environment", "-e", default="staging", help="The person to greet")
+def get_user_info(environment, auth_mode):
+    """Show user info information."""
+    environment = DeploymentEnvironment(environment)
+    access_token = obi_auth.get_token(environment=environment, auth_mode=AuthMode(auth_mode))
+    print(json.dumps(obi_auth.get_user_info(access_token, environment=environment), indent=2))
